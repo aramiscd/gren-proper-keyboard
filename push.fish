225782e6faaa718d8f9d1c9ecfc8aeaeb4711c5b
Aramís Concepción Durán <aramis@systemli.org>
2024-07-11 06:46:17 +0200

    Von Elm auf Gren umwurschteln
    readme
    
    push.fish
    
    push.fish
    
    Fix gren.json

---
+++ push.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/env fish
+
+git push origin
+git push git@git.sr.ht:~aramis/gren-proper-keyboard
+git push git@github.com:aramiscd/gren-proper-keyboard
+
+git push --tags origin
+git push --tags git@git.sr.ht:~aramis/gren-proper-keyboard
+git push --tags git@github.com:aramiscd/gren-proper-keyboard
