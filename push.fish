17134c474684bb1bbe98c0c8c06f1d3d91f19d37
Aramís Concepción Durán <aramis@systemli.org>
2024-08-01 07:20:15 +0200

    push.fish

--- push.fish
+++ push.fish
@@ -1,9 +1,10 @@
 #!/usr/bin/env fish
 
 git push origin
-git push git@git.sr.ht:~aramis/gren-proper-keyboard
-git push git@github.com:aramiscd/gren-proper-keyboard
+git push origin --tags
 
-git push --tags origin
-git push --tags git@git.sr.ht:~aramis/gren-proper-keyboard
-git push --tags git@github.com:aramiscd/gren-proper-keyboard
+git push git@git.sr.ht:~aramis/gren-proper-keyboard
+git push git@git.sr.ht:~aramis/gren-proper-keyboard --tags
+
+git push git@github.com:aramiscd/gren-proper-keyboard
+git push git@github.com:aramiscd/gren-proper-keyboard --tags
